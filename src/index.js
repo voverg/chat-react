c2829b36a1d6d3c3f0bd6fc2f6de93927fbd2921
voverg <voverg81@mail.ru>
2021-11-26 17:54:05 +0300

    Connect to Firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
+import {FirebaseContext} from 'context';
 import App from 'App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnKe95fzDiqbokcLS88FmGtBERjapSC2I",
+  authDomain: "chat-react-prog.firebaseapp.com",
+  projectId: "chat-react-prog",
+  storageBucket: "chat-react-prog.appspot.com",
+  messagingSenderId: "400703762312",
+  appId: "1:400703762312:web:06fe4045bde9ec7dc4143d",
+  measurementId: "G-18ZCHKGMEW"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={{
+    firebase,
+    db,
+    collection,
+    getDocs,
+  }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
 
-import {BaseInput, BaseButton} from 'components/ui';
+import {FirebaseContext, AuthContext} from 'context';
+import {BaseInput, BaseButton, Spinner} from 'components/ui';
 
 const Chat = (props) => {
+  const {db, collection, getDocs} = useContext(FirebaseContext);
+  const {user} = useContext(AuthContext);
+  const [value, setValue] = useState('');
+
+  const getMessages = async (db) => {
+    const messagesCol = collection(db, 'messages');
+    const messageSnapshot = await getDocs(messagesCol);
+    const messageList = messageSnapshot.docs.map(doc => doc.data());
+
+    return messageList;
+  }
+
+  const messages = getMessages(db).then((result) => {
+    console.log(result[0]);
+  });
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    console.log(value);
+    setValue('');
+  };
 
   return (
     <div className="chat">
       <div className="chat__dashboard">Chat</div>
 
-      <form className="chat__new-message">
+      <form className="chat__new-message" onSubmit={sendMessage}>
         <BaseInput
           className="chat__input"
           type="text"
           placeholder="Введите сообщение..."
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
         />
         <BaseButton
           className="chat__add-btn"
